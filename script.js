ce90bd401f2f0b28995666d7ca34039faa55de4a
Medvina <medvinastclair@yahoo.com>
2025-06-16 23:27:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Fonction appelée directement depuis l'attribut "onsubmit" dans le HTML
 function validerFormulaire() {
   let prenom =document.getElementById("prenom").value.trim();
   let nom = document.getElementById("nom").value.trim();
@@ -12,20 +11,4 @@ function validerFormulaire() {
 
   alert("Merci " + prenom + " " + nom + ", votre message a bien été envoyé !");
   return true;
-}
-
-
-let projets = document.getElementsByClassName("projet-item");
-
-for (i = 0; i < projets.length; i++) {
-  projets[i].onmouseover = function () {
-    this.style.transform = "scale(1.05)";
-    this.style.transition = "transform 0.3s ease-in-out";
-    this.style.boxShadow = "0 0 15px rgba(0,0,0,0.2)";
-  };
-
-  projets[i].onmouseout = function () {
-    this.style.transform = "scale(1)";
-    this.style.boxShadow = "none";
-  };
 }
