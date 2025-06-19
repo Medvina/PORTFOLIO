fd7a3a05be4a848ca95ea4f5717df1495521fff3
Medvina <medvinastclair@yahoo.com>
2025-06-19 15:30:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function validerFormulaire() {
-  let prenom =document.getElementById("prenom").value.trim();
-  let nom = document.getElementById("nom").value.trim();
-  let projet =document.getElementById("projet").value.trim();
-  let message = document.getElementById("message").value.trim();
-  let email = document.getElementById("email").value.trim();
-  if (!prenom || !nom || !projet || !message) {
-    alert("Veuillez remplir tous les champs du formulaire.");
-    return false;
-  }
 
-  alert("Merci " + prenom + " " + nom + ", votre message a bien été envoyé !");
-  return true;
-}
